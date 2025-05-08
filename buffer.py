80806e2715191eb76afbc81602013628222adddc
tr0ublekat <kirillkojev8@gmail.com>
2025-05-08 22:30:55 +0300

    Убрал копию в буфер и добавил сохранение в папку для obsidian

--- buffer.py
+++ buffer.py
@@ -1,88 +1,94 @@
 #!env/Scripts/python
 
+import datetime
 import subprocess
 import os
-from PIL import Image
-import win32clipboard
-from io import BytesIO
 import keyboard
 import threading
-import time
 
-import win32con
-CF_PNG = win32clipboard.RegisterClipboardFormat("PNG")
+paths = {
+    1: r"D:\education\sem6\lections\network\images",
+    2: r"C:\Users\tr0ublekat\Desktop"
+}
+current_path = None
+
+
+def get_path():
+    global current_path
+    print("Выберите путь для сохранения изображений:")
+    for path in paths:
+        print(str(path) + ": " + paths[path])
+
+    current_path = paths[int(input("Введите номер пути: "))]
+    print(f"Путь для сохранения изображений: {current_path}")
+
 
 def check_device_connected():
     result = subprocess.run(['adb', 'devices'], capture_output=True, text=True)
     return 'device' in result.stdout
 
-def get_latest_photo():
+
+def get_photo(index=1):
     try:
         output = subprocess.check_output(
             ['adb', 'shell', 'ls', '-t', '/sdcard/DCIM/Camera'],
             text=True,
             stderr=subprocess.STDOUT
         ).splitlines()
-        return output[0].strip() if output else None
+        return output[index-1].strip() if output else None
     except subprocess.CalledProcessError as e:
         print(f"Ошибка при получении списка файлов: {e.output}")
         return None
 
+
 def download_file(remote_path, local_path):
     try:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+        file_extension = os.path.splitext(remote_path)[1]  # Получаем расширение файла
+        new_name = f"{timestamp}{file_extension}"
+        local_path = os.path.join(local_path, new_name)
+
         subprocess.run(['adb', 'pull', remote_path, local_path], check=True)
         return True
     except subprocess.CalledProcessError as e:
         print(f"Ошибка при скачивании файла: {e}")
         return False
 
-def copy_image_to_clipboard(image_path):
-    try:
-        with Image.open(image_path) as img:
-            img.thumbnail((1920, 1080))
-            output = BytesIO()
-            # Сохраняем в PNG вместо BMP
-            img.save(output, "PNG", compress_level=9)
-            data = output.getvalue()
-            output.close()
 
-            win32clipboard.OpenClipboard()
-            win32clipboard.EmptyClipboard()
-            # Используем зарегистрированный формат PNG
-            win32clipboard.SetClipboardData(CF_PNG, data)
-            win32clipboard.CloseClipboard()
-            print("Изображение скопировано в буфер обмена!")
-    except Exception as e:
-        print(f"Ошибка при копировании: {e}")
-
-def main():
+def main(index=1):
     if not check_device_connected():
         print("Устройство не найдено. Подключите телефон по USB и включите отладку.")
         return
 
-    latest_photo = get_latest_photo()
-    if not latest_photo:
+    photo = get_photo(index=index)
+    if not photo:
         print("Не удалось найти фотографии в /sdcard/DCIM/Camera")
         return
 
-    remote_path = f"/sdcard/DCIM/Camera/{latest_photo}"
-    local_path = os.path.join(os.getcwd(), f"temp_photo_{time.time()}.jpg")
+    remote_path = f"/sdcard/DCIM/Camera/{photo}"
 
-    if not download_file(remote_path, local_path):
+    if not download_file(remote_path, current_path):
         return
 
-    copy_image_to_clipboard(local_path)
-
-    try:
-        os.remove(local_path)
-    except Exception as e:
-        print(f"Ошибка при удалении временного файла: {e}")
-
-def trigger_main():
-    thread = threading.Thread(target=main)
-    thread.start()
 
 if __name__ == "__main__":
-    keyboard.add_hotkey('ctrl+space', trigger_main)
-    print("Программа запущена. Нажимайте Ctrl+Space для копирования последней фотографии.")
-    keyboard.wait()
+    keyboard.add_hotkey('ctrl+space', main)
+    keyboard.add_hotkey('ctrl+shift+1', main, args=(1,))
+    keyboard.add_hotkey('ctrl+shift+2', main, args=(2,))
+    keyboard.add_hotkey('ctrl+shift+3', main, args=(3,))
+    keyboard.add_hotkey('ctrl+shift+4', main, args=(4,))
+    keyboard.add_hotkey('ctrl+shift+5', main, args=(5,))
+    keyboard.add_hotkey('ctrl+shift+6', main, args=(6,))
+    keyboard.add_hotkey('ctrl+shift+7', main, args=(7,))
+    keyboard.add_hotkey('ctrl+shift+8', main, args=(8,))
+    keyboard.add_hotkey('ctrl+shift+9', main, args=(9,))
+    keyboard.add_hotkey('ctrl+shift+space', lambda: os._exit(0))
+    print("Ctrl+Shift+<number> для копирования <number> фотографии с конца.")
+    print("Ctrl+Space для копирования последней фотографии.")
+
+    try:
+        get_path()
+        keyboard.wait()
+    except KeyboardInterrupt:
+        print("Программа завершена.")
+        exit(0)
